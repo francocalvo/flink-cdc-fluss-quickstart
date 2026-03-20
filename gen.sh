741cf6555c3f7ee6792c45fc4a8223b4a37e9a95
francocalvo <dev@francocalvo.ar>
2026-03-20 18:39:36 -0300

    feat: unify deploy script with config-driven S3 paths
    Replace hardcoded S3 bucket URLs with variables derived from .config.
    Make sync.sh portable by using dirname instead of an absolute path.

--- gen.sh
+++ gen.sh
@@ -1,6 +1,5 @@
+#!/usr/bin/env bash
 nix-shell -p \
-      python312Packages.pandas \
-      python312Packages.sqlalchemy \
-      python312Packages.psycopg2 \
-      python312Packages.faker \
-      --run "python ./postgres-source/gen_data.py $@"
+    python312Packages.psycopg2 \
+    python312Packages.faker \
+    --run "python ./postgres-source/gen_data.py $*"
--- sync.sh
+++ sync.sh
@@ -2,6 +2,6 @@ rsync  \
   -avz --exclude=".venv" --exclude='.git' --exclude='node_modules' --exclude='__pycache__' --exclude='*.pyc' --exclude='.DS_Store' \
   --exclude='streaming-gradle/.gradle' --exclude='streaming-gradle/lib/build' --exclude='streaming-gradle/build' \
   --exclude='streaming-gradle/lib/.gradle' --exclude='streaming-gradle/lib/bin' \
-  /Users/francocalvo/draftea/pocs/streaming/ \
+  "$(dirname "$0")/" \
   calvo@192.168.1.202:/mefitis/streaming/
 
